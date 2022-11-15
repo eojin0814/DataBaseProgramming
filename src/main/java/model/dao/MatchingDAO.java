a506e89d59715d17e529e0fbd5108f18bd776a80
hyebim <shb010424@gmail.com>
2022-11-16 03:36:50 +0900

    dd

--- src/main/java/model/dao/MatchingDAO.java
+++ src/main/java/model/dao/MatchingDAO.java
@@ -19,7 +19,38 @@ private JDBCUtil jdbcUtil = null;
 		jdbcUtil = new JDBCUtil();	// JDBCUtil 객체 생성
 	}
 		
+	public void create(int carShareId, BoardDTO board, ReservationDTO reservation, StationDTO station) throws SQLException {
+	      String sql = "INSERT INTO CARSHARE VALUES (?, ?, ?, ?)";      
+	      Object[] param = new Object[] {carShareId, board.getCarshare_Date,reservation.getReservationId, station.getStationId};            
+	      jdbcUtil.setSqlAndParameters(sql, param);
+	         
+	      try {            
+	         int result = jdbcUtil.executeUpdate();   // insert 臾   떎 뻾
+	         System.out.println(result);
+	         } catch (Exception ex) {
+	         jdbcUtil.rollback();
+	         ex.printStackTrace();
+	      } finally {      
+	         jdbcUtil.commit();
+	         jdbcUtil.close();   // resource 諛섑솚
+	      }   
+	}
 	
-	
+	public void create(int carShareId, BoardDTO board, ReservationDTO reservation, StationDTO station) throws SQLException {
+	      String sql = "INSERT INTO CARSHARE VALUES (?, ?, ?, ?)";      
+	      Object[] param = new Object[] {carShareId, board.getCarshare_Date,reservation.getReservationId, station.getStationId};            
+	      jdbcUtil.setSqlAndParameters(sql, param);
+	         
+	      try {            
+	         int result = jdbcUtil.executeUpdate();   // insert 臾   떎 뻾
+	         System.out.println(result);
+	         } catch (Exception ex) {
+	         jdbcUtil.rollback();
+	         ex.printStackTrace();
+	      } finally {      
+	         jdbcUtil.commit();
+	         jdbcUtil.close();   // resource 諛섑솚
+	      }   
+	}
 
 }
